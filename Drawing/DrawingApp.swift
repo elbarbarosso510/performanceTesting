e62424a8909cdde674058d6210f2276406bf2b67
elbarbarosso510 <el_barbarosso510@protonmail.com>
2024-05-26 16:40:42 +0300

    update translations

---
+++ Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Vladimir on 03.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
